f13966d5b24906601c81ed01debdc94651884bd4
TorchStone <brickbuscemi@gmail.com>
2019-10-25 09:42:15 -0700

    Test

--- Potion_Crafting.py
+++ Potion_Crafting.py
@@ -9,3 +9,4 @@ materialsDB = {"city":
                 1:{"Pitch":{"Tenebrae":3, "Vinculum":1, "Instramentum":2}, "Saltpetre": {"Vitreus":2, "Potentia":3, "Victus":1}, "Phosporus":{"Lux":4, "Metalum":2}},
                 2:{"Quicksilver": {"Metalum":2, "Motus": 4, "Venenum":2}, "Sulfer": {"Ignis":3, "Potentia":3, "Telum":3}}},
                "Wilds":{}}
+change = 'new'
