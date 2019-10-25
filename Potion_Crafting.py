e67133e71554bfc36d865be0a4ee2572ac120370
TorchStone <brickbuscemi@gmail.com>
2019-10-25 09:31:25 -0700

    Prototype Start

---
+++ Potion_Crafting.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 23, 2019
+
+@author: TorchStone
+'''
+
+materialsDB = {"city":
+               {0:{"Milk":{"Terra":2, "Victus": 1}, "Water":{"Aqua":3}, "Salt":{"Terra":1, "Vitreus":2}},
+                1:{"Pitch":{"Tenebrae":3, "Vinculum":1, "Instramentum":2}, "Saltpetre": {"Vitreus":2, "Potentia":3, "Victus":1}, "Phosporus":{"Lux":4, "Metalum":2}},
+                2:{"Quicksilver": {"Metalum":2, "Motus": 4, "Venenum":2}, "Sulfer": {"Ignis":3, "Potentia":3, "Telum":3}}},
+               "Wilds":{}}
